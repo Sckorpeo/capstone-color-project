5c4c1178ae5c4d15ea834d620f24a9085a48f09d
Kieran B <kieranbru.web@gmail.com>
2021-08-18 13:24:28 -0400

    Added toggle for color format
    Added toggle for color format and notification when format is changed.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Slider, { Range } from 'rc-slider';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
@@ -9,7 +11,7 @@ class Navbar extends Component {
         this.state = {}
     }
     render() {
-        const { level, changeLevel } = this.props
+        const { level, changeLevel, changeFormat, format } = this.props
         return (
             <nav className='Navbar'>
                 <div className='logo'>
@@ -33,6 +35,14 @@ class Navbar extends Component {
                             }}
                         />
                     </div>
+                </div>
+                <div className='select-container'>
+                    <Select onChange={changeFormat}
+                        value={format}>
+                        <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+                        <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0) </MenuItem>
+                    </Select>
                 </div>
             </nav>
         );
--- src/Palette.js
+++ src/Palette.js
@@ -1,31 +1,73 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import './Palette.css';
 
 class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 };
+        this.state = {
+            level: 500,
+            format: 'hex',
+            open: false
+        };
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
     changeLevel(level) {
         console.log(level)
         this.setState({ level })
     }
+    changeFormat(evt) {
+        this.setState({
+            format: evt.target.value,
+            open: true
+        })
+    }
+    closeSnackbar(evt) {
+        this.setState({
+            open: false
+        })
+    }
 
     render() {
         const { colors } = this.props.palette;
-        const { level } = this.state;
+        const { level, format, open } = this.state;
         const colorBoxes = colors[level].map(color => {
-            return <ColorBox background={color.hex} name={color.name} />
+            return <ColorBox background={color[format]} name={color.name} />
         });
         return (
             <div className='Palette'>
-                <Navbar level={level} changeLevel={this.changeLevel} />
+                <Navbar level={level}
+                    changeLevel={this.changeLevel}
+                    changeFormat={this.changeFormat}
+                    format={format} />
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                    open={open}
+                    autoHideDuration={3000}
+                    message={<span id='message-id'>Format Changed</span>}
+                    ContentProps={{
+                        'aria-describedby': 'message-id'
+                    }}
+                    action={[
+                        <IconButton onClick={this.closeSnackbar}
+                            color='inherit'
+                            key='close'
+                            aria-label='close notification'
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                    onClose={this.closeSnackbar}
+                />
             </div>
         );
     }
