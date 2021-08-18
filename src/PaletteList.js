a39f986920f3b3c77b887cccb2e5eaaf8767d92e
Kieran B <kieranbru.web@gmail.com>
2021-08-18 17:49:23 -0400

    Added mini palettes

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+import MiniPalette from './MiniPalette';
+import { Height } from '@material-ui/icons';
+
+const styles = {
+    root: {
+        backgroundColor: 'blue',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    container: {
+        width: '50%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap'
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        color: 'white'
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3,30%)',
+        gridGap: '5%'
+    }
+}
 
 class PaletteList extends Component {
     constructor(props) {
@@ -7,13 +40,19 @@ class PaletteList extends Component {
         this.state = {}
     }
     render() {
-        const { palettes } = this.props;
+        const { palettes, classes } = this.props;
         return (
-            <div>
-                <h1>Palette List</h1>
-                {palettes.map((p) => <Link to={`/palette/${p.id}`}>{p.paletteName}</Link>)}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1 >Palette List</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map((p) => <Link to={`/palette/${p.id}`}><MiniPalette {...p} /></Link>)}
+                    </div>
+                </div>
             </div>);
     }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
