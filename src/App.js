27449fb7fc2942e499450cddae3c2f3ac739bc92
Kieran B <kieranbru.web@gmail.com>
2021-08-23 17:23:14 -0400

    Added create a palette route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { generatePalette } from './colorHelpers';
 import { Switch, Route } from 'react-router-dom';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
     // const palette = generatePalette(seedColors[4]);
     return (
       <Switch>
+        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
         <Route exact
           path={'/palette/:paletteId/:colorId'}
           render={(routeProps) => <SingleColorPalette
