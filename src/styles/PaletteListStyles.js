6fef41aa7a4710d3fae85a49fb7c015f137b5287
Kieran B <kieranbru.web@gmail.com>
2021-08-26 14:09:52 -0400

    Added svg background

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,17 +1,29 @@
+import { urlToRequest } from 'loader-utils';
+import { down } from './size';
+import bg from './bg.svg'
 const styles = {
     root: {
-        backgroundColor: 'blue',
         height: '100vh',
         display: 'flex',
         alignItems: 'flex-start',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        // background by SVGBackgrounds.com 
+        backgroundColor: '#1C34EE',
+        backgroundImage: `url(${bg})`,
+        overflow: 'scroll'
     },
     container: {
         width: '50%',
         display: 'flex',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        [down('lg')]: {
+            width: '80%'
+        },
+        [down('xs')]: {
+            width: '60%'
+        }
     },
     nav: {
         display: 'flex',
@@ -28,7 +40,16 @@ const styles = {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3,30%)',
-        gridGap: '5%'
+        gridGap: '2.5rem',
+        [down('md')]: {
+            gridTemplateColumns: 'repeat(2,50%)'
+        },
+        [down('xs')]: {
+            gridTemplateColumns: 'repeat(1,100%)'
+        }
+    },
+    heading: {
+        fontSize: '2rem'
     }
 }
 
